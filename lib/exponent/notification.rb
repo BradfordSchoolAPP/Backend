fbb77af5aad8c83bbb758246ed14bf2f2c291ccc
kevin-alvarez <kebingo12@gmail.com>
2018-12-23 11:28:42 -0300

    Notification lib for array of tokens

--- lib/exponent/notification.rb
+++ lib/exponent/notification.rb
@@ -2,12 +2,15 @@ module Exponent
   class Notification
     @client = Exponent::Push::Client.new
 
-    def self.send(token, subject, msg)
-      message = [{
-        to: "ExponentPushToken[#{token}]",
-        title: subject,
-        body: msg
-      }]
+    def self.send(tokens, subject, msg)
+      message = []
+      tokens.collect do |t|
+        message << {
+          to: "ExponentPushToken[#{t}]",
+          title: subject,
+          body: msg
+        }
+      end
       @client.publish message
     end
   end
