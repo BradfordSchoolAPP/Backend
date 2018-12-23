f49a1ad43de2be23876d326112df4c970d7b195f
kevin-alvarez <kebingo12@gmail.com>
2018-12-23 14:09:30 -0300

    Added notifications for important news

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,8 @@
 class Device < ApplicationRecord
-	has_and_belongs_to_many :parents  
+	has_and_belongs_to_many :parents
+	
+	def self.notification_all(params)
+		tokens = all.collect(&:token)
+		Exponent::Notification.send(tokens, 'Nueva Noticia!', params[:title])
+	end
 end
